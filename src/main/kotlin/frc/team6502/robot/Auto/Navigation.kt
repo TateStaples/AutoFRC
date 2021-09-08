c96c53bba4c92e0568f01b621b8c20bfe8ba4a45
TateStaples <56731352+TateStaples@users.noreply.github.com>
2021-09-08 11:05:23 -0400

    Revert "Revert "small tweaks""
    This reverts commit 808c93166509ab603fcea312de613b49974ff6b4.

--- src/main/kotlin/frc/team6502/robot/Auto/Navigation.kt
+++ src/main/kotlin/frc/team6502/robot/Auto/Navigation.kt
@@ -16,10 +16,12 @@ import edu.wpi.first.wpilibj2.command.RamseteCommand
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpiutil.math.MatBuilder
 import edu.wpi.first.wpiutil.math.numbers.*
+import frc.team6502.kyberlib.math.units.extensions.degrees
 import frc.team6502.kyberlib.math.units.extensions.feet
 import frc.team6502.kyberlib.math.units.extensions.meters
 import frc.team6502.kyberlib.math.units.extensions.metersPerSecond
 import frc.team6502.robot.Constants
+import frc.team6502.robot.RobotContainer
 import frc.team6502.robot.subsystems.Drivetrain
 import java.util.*
 
@@ -84,7 +86,7 @@ class Navigation(initialPose: Pose2d) : SubsystemBase() {
         return TrajectoryGenerator.generateTrajectory(
             pose,
             waypoints,
-            Pose2d(waypoints.last(), finalRotation), // TODO figure out how to not set heading
+            Pose2d(waypoints.last(), finalRotation),
             pathingConfig
         )
     }
@@ -123,15 +125,16 @@ class Navigation(initialPose: Pose2d) : SubsystemBase() {
     override fun periodic() {
         update()
         field.robotPose = pose
-        if (Constants.DEBUG) {  // TODO: figure out what goes here
+        if (Constants.DEBUG) {
             SmartDashboard.putString("Goal", currentGoal?.name)
         }
     }
 
     // ----- public variables ----- //
     // location
-    val heading  // what direction the robot is facing
-        get() = Rotation2d(0.0) // TODO RobotContainer.gyro
+    var heading  // what direction the robot is facing
+        get() = Rotation2d(RobotContainer.gyro.fusedHeading.degrees.value)
+        set(value) {RobotContainer.gyro.fusedHeading = value.degrees}
     var pose  // the location and direction of the robot
         get() = if (Constants.MECANUM) mecEstimator.estimatedPosition else difEstimator.estimatedPosition
         set(value) {
