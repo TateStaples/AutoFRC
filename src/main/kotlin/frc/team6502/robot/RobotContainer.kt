094afca12f3d1784e0f60ad8a7fccf3fac74da02
21brownz <21brownz@da.org>
2021-09-21 11:17:20 -0400

    prevent error in nav intialization by creating gyro first

--- src/main/kotlin/frc/team6502/robot/RobotContainer.kt
+++ src/main/kotlin/frc/team6502/robot/RobotContainer.kt
@@ -3,7 +3,6 @@ package frc.team6502.robot
 import kyberlib.input.controller.KXboxController
 import com.ctre.phoenix.sensors.PigeonIMU
 import edu.wpi.first.wpilibj.geometry.Pose2d
-import edu.wpi.first.wpilibj.geometry.Rotation2d
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import frc.team6502.robot.auto.Navigation
 import frc.team6502.robot.auto.pathing.PathPlanner
@@ -15,13 +14,12 @@ import kotlin.math.PI
  * Contains all Robot subsystems and sensors
  */
 object RobotContainer {
+    val gyro = PigeonIMU(Constants.PIGEON_PORT)
     /**
      * The location manager of the robot. Tracks location and odometry updates.
      * Also does some pathing.
      */
     val navigation = Navigation(Pose2d(0.0, 0.0, 0.degrees))
-
-    val gyro = PigeonIMU(Constants.PIGEON_PORT)
 
     /**
      * The main user input device of robot
