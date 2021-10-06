9068b054f84254ea7f6d2b827db0df68fcd148b9
21brownz <21brownz@da.org>
2021-10-06 14:23:24 -0400

    utils mods

--- src/main/kotlin/kyberlib/math/units/WpiLibModifications.kt
+++ src/main/kotlin/kyberlib/math/units/WpiLibModifications.kt
@@ -8,7 +8,7 @@ import kyberlib.math.units.extensions.*
 import kotlin.math.sqrt
 
 val Pose2d.string: String
-    get() = "pose(${this.x}, ${this.y}, ${this.rotation.degrees}"
+    get() = "pose(${this.x}, ${this.y}, ${this.rotation.degrees})"
 fun Pose2d(x: Length, y:Length, rotation: Angle): Pose2d = Pose2d(x.meters, y.meters, rotation)
 val Pose2d.transform
     get() = this.minus(zeroPose)
@@ -16,7 +16,7 @@ val Pose2d.transform
 val zeroPose = Pose2d(0.0, 0.0, 0.degrees)
 
 val Translation2d.string: String
-    get() = "pos(${this.x}, ${this.y}"
+    get() = "trans(${this.x}, ${this.y})"
 
 fun Translation2d(x: Length, y:Length): Translation2d = Translation2d(x.meters, y.meters)
 fun Translation2d(x: Length, rotation: Rotation2d): Translation2d = Translation2d(x.meters, rotation)
