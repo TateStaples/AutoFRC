37614d7c3efa0df14ece72d20c4a0cb4e8e8d334
TateStaples <56731352+TateStaples@users.noreply.github.com>
2021-10-13 09:38:11 -0400

    untested swerve and motorcontrol updates

--- src/main/kotlin/kyberlib/motorcontrol/swerve/SwerveModule.kt
+++ src/main/kotlin/kyberlib/motorcontrol/swerve/SwerveModule.kt
@@ -3,36 +3,30 @@ package kyberlib.motorcontrol.swerve
 import edu.wpi.first.wpilibj.geometry.Rotation2d
 import edu.wpi.first.wpilibj.geometry.Translation2d
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState
-import frc.team6502.kyberlib.motorcontrol.KMotorController
+import kyberlib.math.units.extensions.LinearVelocity
 import kyberlib.math.units.extensions.degrees
 import kyberlib.math.units.extensions.k
 import kyberlib.math.units.extensions.metersPerSecond
 
-open class SwerveModule (val location: Translation2d, val driveMotor: KMotorController, val turnMotor: KMotorController) {
-    // turn controls
-    private var rotation
-        get() = turnMotor.position.normalized
-        set(value) {
-            turnMotor.positionSetpoint = value  // TODO: No idea if this works
-        }
+abstract class SwerveModule(val location: Translation2d) {
+    abstract var rotation: Rotation2d
+    abstract var speed: LinearVelocity
 
-    // drive info
-    private var speed
-        get() = driveMotor.linearVelocity
-        set(value) {driveMotor.linearVelocity = value}
-    private var driveVoltage
-        get() = driveMotor.voltage
-        set(value) {driveMotor.voltage = value}
-
-    // general info vars
-    var state
+    var state: SwerveModuleState
         get() = SwerveModuleState(speed.metersPerSecond, rotation)
         set(value) {
             val optimized = SwerveModuleState.optimize(value, rotation)
+            stateSetpoint = optimized
             rotation = optimized.angle.k
             speed = optimized.speedMetersPerSecond.metersPerSecond
         }
 
-    val breakState
+    var stateSetpoint: SwerveModuleState = breakState
+        private set
+
+    val breakState: SwerveModuleState
         get() = SwerveModuleState(0.0, Rotation2d(location.x, location.y).rotateBy(90.degrees))
+
+
+    abstract fun debug()
 }
