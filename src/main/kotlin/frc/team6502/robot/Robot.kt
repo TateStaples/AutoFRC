534fcdbfed84f3ad17c88cd8289bb5e56891d1cc
TateStaples <56731352+TateStaples@users.noreply.github.com>
2021-10-26 19:58:08 -0400

    Networktable -> json interface

--- src/main/kotlin/frc/team6502/robot/Robot.kt
+++ src/main/kotlin/frc/team6502/robot/Robot.kt
@@ -1,10 +1,12 @@
 package frc.team6502.robot
 
+import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj.geometry.Translation2d
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator
 import frc.team6502.robot.auto.Navigation
 import frc.team6502.robot.commands.drive.AutoDrive
 import frc.team6502.robot.commands.general.CommandManager
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import kyberlib.command.KRobot
 import kyberlib.math.units.zeroPose
 
@@ -20,14 +22,39 @@ class Robot : KRobot() {
 
     override fun autonomousInit() {
         Constants.AUTO = true
-        val drive1 = AutoDrive(Translation2d(3.0, 0.0))  // Drives here
+        val drive1 = AutoDrive(Translation2d(0.0, 3.0))  // Drives here
 //        val drive2 = AutoDrive(Translation2d(10.0, 3.0))  // drives here
-        val traj = Navigation.trajectory(Translation2d(3.0, 0.0), Translation2d(3.0, 3.0))
-        CommandManager.enqueue(AutoDrive(traj))
+        CommandManager.enqueue(drive1)
     }
 
     override fun teleopInit() {
         Constants.AUTO = false
         Navigation.pose = zeroPose
     }
+
+    private val slamValues = "slamValues.json"
+    private val table = NetworkTableInstance.getDefault().getTable("SLAM")
+    private val xEntry = table.getEntry("X")
+    private val yEntry = table.getEntry("Y")
+    private val thetaEntry = table.getEntry("THETA")
+    private val outputTimeEntry = table.getEntry("OUTPUT TIME")
+    private var prevOutputTime = -1
+
+    override fun simulationInit() {
+    }
+
+    override fun simulationPeriodic() {
+        val deserialized = Json.decodeFromString<SlamValues>(slamValues)
+        if (prevOutputTime == deserialized.outputImgTime) return
+        prevOutputTime = deserialized.outputImgTime
+        xEntry.setNumber(deserialized.X)
+        yEntry.setNumber(deserialized.Y)
+        thetaEntry.setNumber(deserialized.THETA)
+        outputTimeEntry.setNumber(deserialized.outputImgTime)
+    }
 }
+
+data class SlamValues(
+    val X:Double, val Y: Double, val THETA: Double, val outputImgTime: Int,
+    val inputImgTime: Int
+)
