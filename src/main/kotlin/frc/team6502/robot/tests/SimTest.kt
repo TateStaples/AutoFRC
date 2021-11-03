8f2e62892ff09899591cb26a48a7e8dd08ae4b6c
Tate Staples <tatestaples@icloud.com>
2021-11-02 21:11:51 -0400

    Sim works!

--- src/main/kotlin/frc/team6502/robot/tests/SimTest.kt
+++ src/main/kotlin/frc/team6502/robot/tests/SimTest.kt
@@ -1,18 +1,14 @@
 package frc.team6502.robot.tests
 
-import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
-import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator
 import frc.team6502.robot.Constants
 import kyberlib.command.KRobot
 import kyberlib.input.controller.KXboxController
-import kyberlib.math.units.Pose2d
-import kyberlib.math.units.Translation2d
-import kyberlib.math.units.extensions.*
-import kyberlib.math.units.zeroPose
+import kyberlib.math.units.extensions.feet
+import kyberlib.math.units.extensions.inches
+import kyberlib.math.units.extensions.metersPerSecond
 import kyberlib.mechanisms.drivetrain.DifferentialDriveConfigs
 import kyberlib.mechanisms.drivetrain.DifferentialDriveTrain
 import kyberlib.motorcontrol.KSimulatedESC
@@ -41,12 +37,13 @@ class SimTest : KRobot() {
         driveTrain.setupSim(Constants.DRIVE_KV, Constants.DRIVE_KA, 1.5, 0.3)  // this is a physical representation of the drivetrain
         driveTrain.drive(ChassisSpeeds(1.0, 0.0, 0.0))  // starts it driving forward
         Simulation.include(driveTrain)  // this will periodically update
+        driveTrain.controller = controller
 //        driveTrain.pose = Pose2d(2.meters, 2.meters, 0.degrees)
     }
 
     override fun simulationPeriodic() {
-        val forward = -controller.leftY.value
-        val turn = -controller.rightX.value
+        val forward = -controller.leftY.value * Constants.velocity.metersPerSecond
+        val turn = -controller.rightX.value * 3.0
         SmartDashboard.putNumber("forward", forward)
         SmartDashboard.putNumber("turn", turn)
         driveTrain.drive(ChassisSpeeds(forward * Constants.velocity.metersPerSecond, 0.0, turn))
