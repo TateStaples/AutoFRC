277e0e97c171f8dd991763f52c573b260822e7bd
Tate Staples <tatestaples@icloud.com>
2021-11-05 17:57:40 -0400

    integrating.

--- src/main/kotlin/frc/team6502/robot/tests/Integration.kt
+++ src/main/kotlin/frc/team6502/robot/tests/Integration.kt
@@ -1,14 +1,18 @@
 package frc.team6502.robot.tests
 
+import edu.wpi.first.wpilibj.geometry.Translation2d
 import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import frc.team6502.robot.Constants
+import frc.team6502.robot.auto.Navigation
+import frc.team6502.robot.commands.balls.Intake
+import frc.team6502.robot.commands.drive.AutoDrive
 import frc.team6502.robot.subsystems.Drive
 import kyberlib.auto.Navigator
+import kyberlib.command.CommandManager
 import kyberlib.command.KRobot
 import kyberlib.input.controller.KXboxController
-import kyberlib.math.units.extensions.metersPerSecond
+import kyberlib.math.units.zeroPose
 import kyberlib.sensors.gyros.KPigeon
 import kyberlib.simulation.Simulation
 import kyberlib.simulation.field.KField2d
@@ -41,6 +45,8 @@ class Integration : KRobot() {
     val navigation = Navigator(gyro)
 
     override fun teleopPeriodic() {
+        CommandManager.clear()
+        Constants.AUTO = false
         val forward = controller.leftY.value
         val turn = controller.rightX.value
         SmartDashboard.putNumber("forward", forward)
@@ -48,16 +54,29 @@ class Integration : KRobot() {
         Drive.chassisSpeeds = ChassisSpeeds(forward, 0.0, turn)
     }
 
+    override fun autonomousPeriodic() {
+        Constants.AUTO = true
+        Navigation.pose = zeroPose
+        val traj = navigation.trajectory(Translation2d(1.0,0.0), Translation2d(1.0, 2.0))
+        KField2d.getObject("traj").setTrajectory(traj)
+//        val drive1 = AutoDrive(traj)  // Drives here
+//        val seq = CommandManager.sequence(drive1, Intake())
+//        val drive2 = AutoDrive(Translation2d(10.0, 3.0))  // drives here
+//        drive1.schedule()
+//        CommandManager.clear()
+//        CommandManager.enqueue(drive1)
+//        CommandManager.enqueue(Intake())
+    }
+
     override fun robotPeriodic() {
         KField2d.robotPose = navigation.pose
     }
 
+    override fun simulationInit() {
+//        Drive.defaultCommand = AutoDrive(Translation2d(3.0, 3.0))
+    }
+
     override fun simulationPeriodic() {
-        val forward = controller.leftY.value.coerceAtMost(Constants.velocity.metersPerSecond)
-        val turn = controller.rightX.value
-        SmartDashboard.putNumber("forward", forward)
-        SmartDashboard.putNumber("turn", turn)
-        Drive.chassisSpeeds = ChassisSpeeds(forward, 0.0, turn)
     }
 
 }
