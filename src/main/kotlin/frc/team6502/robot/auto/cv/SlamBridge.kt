926e0375d0ad114da239df9f4e201a98db3f92b4
Tate Staples <tatestaples@icloud.com>
2021-11-19 13:20:39 -0500

    trying to calibrate - pain

--- src/main/kotlin/frc/team6502/robot/auto/cv/SlamBridge.kt
+++ src/main/kotlin/frc/team6502/robot/auto/cv/SlamBridge.kt
@@ -10,12 +10,12 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kyberlib.command.Debug
-import org.opencv.core.Core
-import org.opencv.core.Mat
-import org.opencv.core.Size
+import org.opencv.calib3d.Calib3d
+import org.opencv.core.*
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
 import java.io.File
+
 
 /**
  * Bridge between the robot and UcoSlam algorithm.
@@ -51,17 +51,50 @@ class SlamBridge : SubsystemBase() {
         }
     }
 
-    var index = 0
-    fun save_image() {
+    var found = 0
+
+    val objectPoints = mutableListOf<Mat>()
+    val imagePoints = mutableListOf<Mat>()
+    fun calibrate() {
+        println("found $found images")
+        if (found > 10) {
+            val outputMatrix = Mat()
+            val dist = Mat()
+            val rvecs = mutableListOf<Mat>()
+            val tvecs = mutableListOf<Mat>()
+            Calib3d.calibrateCamera(objectPoints, imagePoints, Size(640.0, 480.0),outputMatrix, dist, rvecs, tvecs)
+            println("Camera Matrix: $outputMatrix")
+            println("dist: $dist")
+            println("r: $rvecs")
+            println("t: $tvecs")
+            0/0
+        }
         val mat = Mat()
         CameraServer.getInstance().video.grabFrame(mat)
-        Imgcodecs.imwrite("./UcoSlam/calibration/image$index.jpg", mat)
-        println("here to images/image$index.jpg")
-        index++
+        if (mat.empty()) return
+        val gray = Mat()
+        Imgproc.cvtColor(mat, gray, Imgproc.COLOR_BGR2GRAY)
+        val corners = MatOfPoint2f()
+        val hasBoard = Calib3d.findChessboardCorners(gray, Size(6.0, 8.0), corners)
+        if (hasBoard) {
+            found++
+            var points: MatOfPoint3f
+
+            val a: Mat = MatOfPoint3f()
+            for (y in 0 until 6) {
+                for (x in 0 until 8) {
+                    points = MatOfPoint3f(Point3(x.toDouble(), y.toDouble(), 0.0))
+                    a.push_back(points)
+                }
+            }
+            objectPoints.add(a)
+            imagePoints.add(corners)
+
+        }
     }
 
     override fun periodic() {
-        save_image()
+        calibrate()
         return
         // read to SLAM output
         var deserialized = Json.decodeFromString<SlamValues>(slamValues.readText())
